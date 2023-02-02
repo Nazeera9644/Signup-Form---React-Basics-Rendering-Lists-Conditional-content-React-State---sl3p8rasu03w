9b6e07af5fa49c3d8500b0a826ae6d966dfb1bbb
Shaik Nazeer Begum <118837825+Nazeera9644@users.noreply.github.com>
2023-02-02 17:45:18 +0530

    Signup Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+function App(){
+
+
   return (
-    <div id="main"></div>
+    <div id="main">
+       
+      <form>
+        <input type="text" data-testid='name' placeholder="Name"/>
+        <br/>
+        <input type="text" data-testid='email' placeholder="Email"/>
+        <br/>
+        <select data-testid='gender'>
+          <option>Male</option>
+          <option>Female</option>
+        </select>
+        <br/>
+        <input type="number" data-testid='phoneNumber' placeholder="Phone Number"/>
+        <br/>
+        <input  type="password" data-testid='password' placeholder="Password"/>
+        <br/>
+        <button data-testid='submit'>Submit</button>
+
+      </form>
+    </div>
   )
 }
 
